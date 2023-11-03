f9565dc0a947f41df0e8939c1a7788f212fd83ab
Thomas McPhee <mcp.thom@gmail.com>
2023-11-03 15:14:05 +0000

    added ability to manipulate DOM for Temp, Name,Humidity, and Wind Speed. Still only works for Edinburgh currently.

--- main.js
+++ main.js
@@ -7,7 +7,10 @@ async function checkWeather() {
     let data = await response.json();
     console.log(data);
 
-    // document.querySelector(".city")
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = data.main.temp;
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h"
 }
 
 checkWeather();
