175742e7b232d43678eb74ee9deffbcf4fb0b723
Thomas McPhee <mcp.thom@gmail.com>
2023-11-03 15:04:39 +0000

    added async function with fetch to handle GET request from API. Can only get data from Edinburgh at the moment.

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+// API 
+const apiKey = "a3aaf92221d99400b13470c11c4f0589";
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Edinburgh&units=metric`
+
+async function checkWeather() {
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    let data = await response.json();
+    console.log(data);
+
+    // document.querySelector(".city")
+}
+
+checkWeather();
+
 const form = document.querySelector('form');
 console.log(form);
 
