787121bb95f4c70085ead6a07bdaa899da50e26f
Thomas McPhee <mcp.thom@gmail.com>
2023-12-11 20:09:29 +0000

    Removed API request upon page loading. Added error handling for wrong city input.

--- main.js
+++ main.js
@@ -5,11 +5,13 @@ const apiKey = "a3aaf92221d99400b13470c11c4f0589";
 async function checkWeather(cityName) {
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric`
     const response = await fetch(apiUrl + `&appid=${apiKey}`);
+
+    if (response.status == 404) {
+        document.querySelector('.error').style.display = 'block'
+    }
+
     let data = await response.json();
 
-    if (data) {
-        document.querySelector('.error').classList.remove('showError');
-    }
 
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
@@ -20,8 +22,6 @@ async function checkWeather(cityName) {
     document.querySelector(".weather img").src = `images/${data.weather[0].main.toLowerCase()}.png`
     document.querySelector('.weather').classList.remove('displayWeather');
 }
-
-checkWeather();
 
 const form = document.querySelector('form');
 console.log(form);
