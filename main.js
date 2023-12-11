7c5f9f880474e3c418f51790a46da4459f157d94
Thomas McPhee <mcp.thom@gmail.com>
2023-12-11 15:58:53 +0000

    added ability to update weather image.

--- main.js
+++ main.js
@@ -6,12 +6,14 @@ async function checkWeather(cityName) {
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric`
     const response = await fetch(apiUrl + `&appid=${apiKey}`);
     let data = await response.json();
-    console.log(data);
 
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = Math.round(data.wind.speed) + "km/h"
+
+//    UPDATE IMAGE
+    document.querySelector(".weather img").src = `images/${data.weather[0].main.toLowerCase()}.png`
 }
 
 checkWeather();
@@ -22,8 +24,6 @@ console.log(form);
 // handle form submit
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-    console.log(e.target[0].value);
     const cityName = e.target[0].value;
     checkWeather(cityName)
-    // document.querySelector(".city").innerHTML = e.target[0].value;
 })
