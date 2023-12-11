3e46b2e0e67769f227793b6dbb45db68d9df347f
Thomas McPhee <mcp.thom@gmail.com>
2023-12-11 15:38:54 +0000

    Absolutely no idea where I am currently but forgot to commit last update

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-// API 
-const apiKey = "";
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Edinburgh&units=metric`
 
-async function checkWeather() {
+// API
+const apiKey = "a3aaf92221d99400b13470c11c4f0589";
+
+async function checkWeather(cityName) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric`
     const response = await fetch(apiUrl + `&appid=${apiKey}`);
     let data = await response.json();
     console.log(data);
 
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = data.main.temp;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h"
+    document.querySelector(".wind").innerHTML = Math.round(data.wind.speed) + "km/h"
 }
 
 checkWeather();
@@ -21,4 +22,8 @@ console.log(form);
 // handle form submit
 form.addEventListener('submit', function(e) {
     e.preventDefault();
+    console.log(e.target[0].value);
+    const cityName = e.target[0].value;
+    checkWeather(cityName)
+    // document.querySelector(".city").innerHTML = e.target[0].value;
 })
