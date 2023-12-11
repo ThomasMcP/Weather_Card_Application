0750231b1eb4f4e00ea6e109686221793f2a9ac1
Thomas McPhee <mcp.thom@gmail.com>
2023-12-11 22:10:19 +0000

    fixed form submission error via search button click to be equal to 'enter' submission.

--- main.js
+++ main.js
@@ -6,17 +6,19 @@ async function checkWeather(cityName) {
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric`
     const response = await fetch(apiUrl + `&appid=${apiKey}`);
 
-    if (response.status == 404) {
-        document.querySelector('.error').style.display = 'block'
-    }
+    if (response.status == 404 || response.status == 400) {
+        document.querySelector('.weather').style.display = 'none';
+        document.querySelector('.error').style.display = 'block';
+    } 
 
     let data = await response.json();
-
 
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = Math.round(data.wind.speed) + "km/h"
+
+    document.querySelector('.error').style.display = 'none';
 
 //    UPDATE IMAGE
     document.querySelector(".weather img").src = `images/${data.weather[0].main.toLowerCase()}.png`
@@ -24,11 +26,11 @@ async function checkWeather(cityName) {
 }
 
 const form = document.querySelector('form');
-console.log(form);
 
 // handle form submit
 form.addEventListener('submit', function(e) {
     e.preventDefault();
     const cityName = e.target[0].value;
     checkWeather(cityName)
+    document.querySelector('.weather').style.display = 'block';
 })
